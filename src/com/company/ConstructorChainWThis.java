dd8487cd06867c8ac0c3b62c1fecc272dd944838
suhail34 <suhailkna@gmail.com>
2022-01-14 18:40:33 +0530

    Real Usage Of constructor Chaining

---
+++ src/com/company/ConstructorChainWThis.java
@@ -0,0 +1,35 @@
+package com.company;
+
+class Chain{
+    int num1;
+    int num2;
+    int num3;
+
+    Chain(int num1, int num2){
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    Chain(int num1, int num2, int num3){
+        this(num1,num2);
+        this.num3 = num3;
+    }
+
+    void avg(){
+        if(num3==0){
+            System.out.println((num1+num2)/2);
+        }else {
+            System.out.println((num1+num2+num3)/3);
+        }
+    }
+}
+
+public class ConstructorChainWThis {
+    public static void main(String[] args) {
+        Chain c1 = new Chain(4, 5);
+        Chain c2 = new Chain(6, 3, 8);
+
+        c1.avg();
+        c2.avg();
+    }
+}
