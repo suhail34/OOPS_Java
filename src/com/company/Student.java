959a8ee73066ea2ebcd70fc1c5d5b217737f8b38
suhail34 <suhailkna@gmail.com>
2022-01-14 17:48:57 +0530

    Invoking current class method via this keyword

--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -8,6 +8,8 @@ public class Student {
     void insertRecord(int rollNo, String name){
         this.rollNo = rollNo;
         this.name = name;
+        displayDetails();//compiler automatically adds this keyword while invoking the method
+        //this.displayDetails();
     }
 
     void displayDetails(){
@@ -31,14 +33,17 @@ class Teacher {
 //        s2.name = "Talha Ansari";
 //        s2.rollNo = 5119001;
         s1.insertRecord(7119012,"Suhail Khan");
+        System.out.println(t1.name+" is teaching cass to "+t1.teachNo+" students");
+
         s2.insertRecord(3119012, "Burhanuddin Fatehi");
+        System.out.println(t2.name+" is teaching DSIP to "+t2.teachNo+" students");
 
 
 //        System.out.println(s1.name+" is learing IOT Roll No "+s1.rollNo);
-        s1.displayDetails();
-        System.out.println(t1.name+" is teaching cass to "+t1.teachNo+" students");
+//        s1.displayDetails();
+//        System.out.println(t1.name+" is teaching cass to "+t1.teachNo+" students");
 //        System.out.println(s2.name+" is learning DSIP Roll No "+s2.rollNo);
-        s2.displayDetails();
-        System.out.println(t2.name+" is teaching DSIP to "+t2.teachNo+" students");
+//        s2.displayDetails();
+//        System.out.println(t2.name+" is teaching DSIP to "+t2.teachNo+" students");
     }
 }
