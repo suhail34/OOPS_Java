0f81dfd2211f3d050961e5e112d224625a754860
suhail34 <suhailkna@gmail.com>
2022-01-20 16:21:18 +0530

    Using variables in Interface

--- src/com/company/Interface.java
+++ src/com/company/Interface.java
@@ -3,14 +3,15 @@ package com.company;
 interface Implement{
     int sum(int nums1, int nums2);
     int subtract(int nums1, int nums2);
+    int FINALANDSTATICVAR = 10;
 }
 
 class Operation implements Implement{
     public int sum(int nums1, int nums2){
         return nums1+nums2;
     }
-
     public int subtract(int nums1, int nums2){
+        System.out.println(Implement.FINALANDSTATICVAR);
         return nums1-nums2;
     }
 }
