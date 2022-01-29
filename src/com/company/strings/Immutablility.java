0257eb3508e1dd3567e965727fe1ac13d2629f00
suhail34 <suhailkna@gmail.com>
2022-01-29 16:56:46 +0530

    More Explanation added w.r.t Immutability of java

--- src/com/company/strings/Immutablility.java
+++ src/com/company/strings/Immutablility.java
@@ -3,8 +3,8 @@ package com.company.strings;
 public class Immutablility {
     public static void main(String[] args) {
         String s = "Hello";
-        s.concat("World");  //this will create a value in string constant pool "World" but will not refer it
-        s = s.concat(" World"); // Now it will concatenate " World" with "Hello" means "Hello World" and will refer to it
+        s.concat("World");  //this will create a object with value in string constant pool "World" but will not refer it
+        s = s.concat(" World"); // Now it will concatenate " World" with "Hello" and create a new object with value means "Hello World" and will refer to it
 
         System.out.println(s);
 //        s.charAt(1) = 'u';  since, java string are immutable
